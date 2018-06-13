9f96523a3a41987d0cf7d640e63cecd16816b7a2
massbath <clement.vassant@gmail.com>
2018-06-13 17:00:40 +0200

    Initialisation du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+constructor(){
+  // Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyAnvroHBp0TulmPODVljjKrr6c11QYqeHs",
+   authDomain: "bookstore-fb8d8.firebaseapp.com",
+   databaseURL: "https://bookstore-fb8d8.firebaseio.com",
+   projectId: "bookstore-fb8d8",
+   storageBucket: "",
+   messagingSenderId: "812544879084"
+ };
+ firebase.initializeApp(config);
+}
+
 }
