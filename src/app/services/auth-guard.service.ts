84ba6926113ef5bb1cf9e73c7cfe3ca930c90ec5
massbath <clement.vassant@gmail.com>
2018-06-14 16:09:36 +0200

    Mise en place de la partie authentification, ajout utilisateur, déconnexion

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot,Router} from '@angular/router';
+import { Observable} from 'rxjs/Observable';
+import * as firebase from 'firebase';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuardService {
+@Injectable()
+export class AuthGuardService implements CanActivate{
 
-  constructor() { }
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state : RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return new Promise(
+        (resolve, reject) => {
+            firebase.auth().onAuthStateChanged(
+               (user) =>{
+                 if(user){
+                   resolve(true);
+                 }else{
+                   this.router.navigate(['/auth','signin']);
+                   resolve(false);
+                 }
+               }
+            );
+        }
+      )
+  }
+
 }
