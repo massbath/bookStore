692db60956a6b2e52ab0138f3acc5525d646d731
massbath <clement.vassant@gmail.com>
2018-06-15 10:38:06 +0200

    Correction d'un bug sur le template html du composant affichantle détail d'un livre

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -3,6 +3,10 @@ import {Book} from '../../models/book.model';
 import {ActivatedRoute, Router} from '@angular/router';
 import {BooksService} from '../../services/books.service';
 
+
+/**
+*Composant affichant le détail d'un livre
+**/
 @Component({
   selector: 'app-single-book',
   templateUrl: './single-book.component.html',
@@ -12,9 +16,14 @@ export class SingleBookComponent implements OnInit {
 
   book: Book;
 
-  constructor(private route: ActivatedRoute, private booksService : BooksService
+  constructor(private route: ActivatedRoute, private booksService : BooksService,
               private router: Router) { }
 
+
+  /**
+  * A l'initialisation du composant on récupère l'id du livre contenu dans le path, et on demande au services
+  * de retourner le livre correspondant de sa collection
+  **/
   ngOnInit() {
     this.book = new Book('','');
     const id = this.route.snapshot.params['id'];
